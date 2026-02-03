b1ac6c31e4766793278ed9a1bfa649c6010ed03f
unknown <vinayak@zeliot.in>
2026-02-03 11:31:20 +0530

    model changed in coldcall.py

--- coldcall.py
+++ coldcall.py
@@ -4,7 +4,7 @@ from condense_context import CONDENSE_CONTEXT
 # from dotenv import load_dotenv
 # load_dotenv()
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-pro",
     temperature=0.2
     
 )
